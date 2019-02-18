423d9aaa19872caec3f396794a1e927f5176a67d
Nithiyanandhan-Sakthivel <47666594+Nithiyanandhan-Sakthivel@users.noreply.github.com>
2019-02-18 22:04:51 +0530

    Update b3vowel.c

--- b3vowel.c
+++ b3vowel.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-int main() {
+#include<ctype.h>
+int main()
+ {
  char i;
  scanf("%c",&i);
+ if(isalpha(i)>0)
+ {
 if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'||i=='A'||i=='E'||i=='I'||i=='O'||i=='U')
 {
   printf("Vowel");
 }
 else
 {
-  printf("Consant");
+  printf("Consonant");
 }
+ }
+ else
+ {
+   printf("invalid");
+ }
 return 0;
 } 
