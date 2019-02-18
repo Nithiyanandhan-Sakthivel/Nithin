70538d5591311fded5cfaa8cbbb3177521f8807c
Nithiyanandhan-Sakthivel <47666594+Nithiyanandhan-Sakthivel@users.noreply.github.com>
2019-02-18 19:42:38 +0530

    Create b1.c

---
+++ b1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+ int i;
+ scanf("%d",&i);
+if(i<0)
+{
+  printf("Negative");
+}
+else if (i>0)
+{
+  printf("Positive");
+}
+else if (i==0)
+{
+  printf("Zero");
+}
+return 0;
+}
