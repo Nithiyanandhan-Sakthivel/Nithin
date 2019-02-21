4492697adc0d33ebca38e805b08a9a863b3ee293
Nithiyanandhan-Sakthivel <47666594+Nithiyanandhan-Sakthivel@users.noreply.github.com>
2019-02-21 19:02:28 +0530

    Create b15.c

---
+++ b15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+ {
+  int n,m,i;
+  scanf("%d%d",&n,&m);
+    for(i=n+1;i<m;i++)
+    {
+      if(i%2==0)
+      {
+        printf("%d\t",i);
+      }
+    }
+  return 0;
+}
