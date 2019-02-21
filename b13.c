072f11d88349db85d68251c9d7c8f2b53b91961a
Nithiyanandhan-Sakthivel <47666594+Nithiyanandhan-Sakthivel@users.noreply.github.com>
2019-02-21 18:29:27 +0530

    Create b13.c

---
+++ b13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+ {
+  int i,n,flag=0;
+  scanf("%d",&n);
+  if(n!=0&&n<=1000){
+  for(i=2;i<=n/2;i++)
+  {
+    if(n%i==0)
+    {
+      flag=1;
+      break;
+    }
+  }
+  if(flag==0)
+  {
+    printf("Yes");
+  }
+  else
+  {
+    printf("No");
+  }
+  }
+  return 0;
+}
