514d5a79e7474743001f403ec2dc1e40731eb8b6
Nithiyanandhan-Sakthivel <47666594+Nithiyanandhan-Sakthivel@users.noreply.github.com>
2019-03-10 20:23:03 +0530

    Create  b25.c

---
+++ b25.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  int i,j,k,n,a[1000],min,temp;
+  scanf("%d",&n);
+  for(k=0;k<n;k++)
+  {
+    scanf("%d",&a[k]);
+  }
+  for(i=0;i<n;i++)
+  {
+    for(j=i+1;j<n;j++)
+    {
+      if(a[i]>a[j])
+      {
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+      }
+    }
+  }
+    printf("%d\t",a[n/2]);
+  return 0;
+}
